7c15b6b42fa3b98188a4ed5dc5fb399abf657d74
Andrea Bianchi <bianan96@gmail.com>
2023-05-20 19:14:24 +0200

    Change homepage to display blog posts

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,78 +1,51 @@
 import React from 'react';
-import Head from 'next/head';
-import Link from 'next/link';
 
-import { getLatestPostsData } from '../lib/posts';
+import { getSortedPostsData } from '/lib/posts';
 
-import { PostCard } from '../components/Thumbnails';
-import Hero from '../components/Hero';
-import Button from '../components/Button';
+import { Post } from '/components/Thumbnails';
+import Hero from '/components/Hero';
 
-import css from '../styles/Home.module.scss';
+import css from '../styles/index.module.scss';
 
 const BLOG = require('../blog.config');
 
 export async function getStaticProps() {
-  const postsData = await getLatestPostsData();
+  const allPostsData = await getSortedPostsData();
 
   return {
-    props: { postsData },
-    revalidate: 10, // In seconds
+    props: { allPostsData },
   };
 }
 
-const Home = ({ postsData }) => {
+const Blog = ({ allPostsData }) => {
   return (
-    <>
-      <Head>
-        <title>Home | {BLOG.NAME}</title>
-        <meta property="og:title" content="homepage" key="title" />
-      </Head>
-      <main>
-        <Hero cname={css.hero} background="transparent">
-          <div className={css.greetings}>
-            <h1>
-              Hello, my name is {BLOG.OWNER.FIRST_NAME} {BLOG.OWNER.LAST_NAME}
-            </h1>
-            <p>
-              Your <span className={css.accent}>catchphrase</span>.
-            </p>
-            <div className={css.CTAs}>
-              <Button className={css.homeCta}>
-                <Link href="/posts">Read my Blog</Link>
-              </Button>
-            </div>
-          </div>
-        </Hero>
-        <section className={css.recentPosts}>
-          <div className={css.title}>
-            <h2>Latest posts</h2>
-          </div>
-          <div className={css.postsContainer}>
-            <ul className={css.posts}>
-              {postsData.map(
-                (
-                  { id, title, description, formattedDate, stats, pic },
-                  index
-                ) => (
-                  <li key={index}>
-                    <PostCard
-                      slug={id}
-                      title={title}
-                      description={description}
-                      date={formattedDate}
-                      stats={stats}
-                      postPic={pic}
-                    />
-                  </li>
-                )
-              )}
-            </ul>
-          </div>
-        </section>
-      </main>
-    </>
+    <main>
+      <Hero cname={css.hero}>
+        <div>
+          <h1 className={css.title}>{BLOG.NAME}</h1>
+          <p>Welcome to my brand-new blog!</p>
+        </div>
+      </Hero>
+      <section className={css.container}>
+        <ul className={css.posts}>
+          {allPostsData.map(
+            ({ id, title, description, formattedDate, stats, pic }, index) => (
+              <li key={index}>
+                <Post
+                  slug={id}
+                  title={title}
+                  description={description}
+                  date={formattedDate}
+                  stats={stats}
+                  postPic={pic}
+                />
+              </li>
+            )
+          )}
+        </ul>
+      </section>
+    </main>
   );
 };
 
-export default Home;
+export default Blog;
