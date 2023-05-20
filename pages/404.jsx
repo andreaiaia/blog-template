7706edd670a4db0f538ab0ec94baff5db0de6c5c
Andrea Bianchi <bianan96@gmail.com>
2023-05-20 19:13:56 +0200

    Fix 404 page

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,16 +1,33 @@
 import React from 'react';
-import Hero from '../components/Hero';
-import css from '../styles/PageNotFound.module.scss';
+import Hero from '/components/Hero';
+import LatestPosts from '/components/LatestPosts';
 
-const pageNotFound = () => {
+import { getLatestPostsData } from '/lib/posts';
+
+import css from '/styles/PageNotFound.module.scss';
+
+export async function getStaticProps() {
+  const postsData = await getLatestPostsData();
+
+  return {
+    props: { postsData },
+    revalidate: 10, // In seconds
+  };
+}
+
+const pageNotFound = ({ postsData }) => {
   return (
     <main>
-      <Hero cname={css.pageNotFound}>
-        <div className={css.title}>
-          <p>404</p>
-          <h1>Page not found</h1>
+      <Hero cname={css.hero}>
+        <div>
+          <h1 className={css.title}>404</h1>
+          <p>Page not found!</p>
         </div>
       </Hero>
+      <LatestPosts
+        posts={postsData}
+        sectionTitle="May I suggest you some readings?"
+      />
     </main>
   );
 };
